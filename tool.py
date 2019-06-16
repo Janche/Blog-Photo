1db4bbf57e822043b0c7f7e210223e53b0de784b
Janche <957671816@qq.com>
2019-06-16 23:11:11 +0800

    更新地址

--- tool.py
+++ tool.py
@@ -130,8 +130,7 @@ def handle_photo():
     list_info.reverse()  # 翻转
     final_dict = {"list": list_info}
 
-    # with open("F:\\vscode_workspace\\janche.github.io\\source\\photos\\data.json","w") as fp:
-    with open("F:\\vscode_workspace\\Blog-Photo\\blog_photos_copy\\data.json","w") as fp:
+    with open("F:\\vscode_workspace\\janche.github.io\\source\\photos\\data.json","w") as fp:
         json.dump(final_dict, fp)
 
 def cut_photo():
